cf5ab9b9bd258b71400d89d68420976909feb772
Abenezer Walelign <walelignabenezer@gmail.com>
2025-10-06 20:51:16 +0300

    Bind port to 0.0.00

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,4 +15,4 @@ echo "✅ Postgres is up—continuing."
 # Now run migrations, collectstatic, and launch server
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-exec gunicorn StyliQ.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn StyliQ.wsgi:application --bind 0.0.0.0:${PORT:-8000}
